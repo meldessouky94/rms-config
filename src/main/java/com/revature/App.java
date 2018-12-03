f46ee3e97a87c007071a3a134ae2df057f842b06
Mitchell Goshorn <msgoshorn@gmail.com>
2018-12-03 02:37:55 -0500

    Initial commit

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class App {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+}
