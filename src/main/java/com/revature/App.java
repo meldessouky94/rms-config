42a819ee40f71fa20fa2049a1aceb8f1f0d9aa0e
zxander <zxanderrodriguez@gmail.com>
2019-01-08 10:22:30 -0500

    adding config for email service

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -3,6 +3,7 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The Class App.
